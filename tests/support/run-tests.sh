783089146e0f83c03d1ce36191ab50da1e5a91d0
Dan Vaida <me@danvaida.com>
2018-08-13 11:52:44 +0200

    OPS-2513 adds the "addition" test case.

--- tests/support/run-tests.sh
+++ tests/support/run-tests.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
 set -e
+ansible-galaxy install -r requirements.yml
+
 ansible-playbook test_defaults.yml
 
 # running a second time to verify playbook's idempotence
 set +e
 ansible-playbook test_defaults.yml > /tmp/second_run.log
+{
+    grep -q 'changed=0.*failed=0' /tmp/second_run.log &&
+    echo 'Playbook is idempotent'
+} || {
+    cat /tmp/second_run.log
+    echo 'Playbook is **NOT** idempotent'
+    exit 1
+}
+
+ansible-playbook test_addition.yml
+
+# running a second time to verify playbook's idempotence
+set +e
+ansible-playbook test_addition.yml > /tmp/second_run.log
 {
     grep -q 'changed=0.*failed=0' /tmp/second_run.log &&
     echo 'Playbook is idempotent'
